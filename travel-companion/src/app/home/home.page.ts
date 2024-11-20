8a208851b04c7aa342937ab5f0054dac3c84e4a0
Colin Williams <123738487+colinwilliamsgithub@users.noreply.github.com>
2024-11-20 16:52:23 +0000

    Added testing features

--- travel-companion/src/app/home/home.page.ts
+++ travel-companion/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
@@ -8,6 +8,34 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  constructor() { }
+
+  async ngOnInit() {
+    await this.getCameraStream();
+    this.getGeolocation();
+    this.getBrowserInfo();
+  }
+
+  async getCameraStream() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      console.log('Camera stream:', stream);
+    } catch (error) {
+      console.error('Error accessing camera:', error);
+    }
+  }
+
+  getGeolocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      console.log('Geolocation:', { latitude, longitude });
+    });
+  }
+
+  getBrowserInfo() {
+    const userAgent = navigator.userAgent;
+    const platform = navigator.platform;
+    console.log('Browser Info:', { userAgent, platform });
+  }
 }
